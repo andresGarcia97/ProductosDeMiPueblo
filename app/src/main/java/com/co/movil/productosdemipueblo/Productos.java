a049aeb08bb92c061f0e086403c2026a11f48310
Duver Carmona <duver.carmona@techandsolve.com>
2020-09-15 14:31:23 -0500

    seccion de productos

---
+++ app/src/main/java/com/co/movil/productosdemipueblo/Productos.java
@@ -0,0 +1,16 @@
+package com.co.movil.productosdemipueblo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.co.movil.productosdemipueblo.R;
+
+public class Productos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_productos);
+    }
+}
